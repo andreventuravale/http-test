f6f1a9d1747530eba2219ac021b6e53a3313f3eb
Andre Vale <andreventuravale@gmail.com>
2024-04-09 15:40:45 -0300

    package.json parser.js pnpm-lock.yaml

--- parser.js
+++ parser.js
@@ -1,84 +1,20 @@
-import { CstParser, Lexer, createToken } from "chevrotain"
+import moo from 'moo'
 
-const Newline = createToken({ name: "Newline", pattern: /\r?\n/ })
-const Space = createToken({ name: "Space", pattern: / +/ })
-const Method = createToken({ name: "Method", pattern: /GET|POST|PUT|DELETE|PATCH|HEAD|OPTIONS/, longer_alt: Space })
-const Url = createToken({ name: "Url", pattern: /https?:\/\/[^\s]+/ })
-const HeaderKey = createToken({ name: "HeaderKey", pattern: /[^\s:]+/ })
-const HeaderValue = createToken({ name: "HeaderValue", pattern: /[^\r\n]+/ })
-const HeaderSeparator = createToken({ name: "HeaderSeparator", pattern: /:/ })
-const TestSeparator = createToken({ name: "TestSeparator", pattern: /###/ })
-
-const allTokens = [Newline, Space, TestSeparator, Method, Url, HeaderKey, HeaderSeparator, HeaderValue]
-
-const HttpLexer = new Lexer(allTokens)
-
-class HttpParser extends CstParser {
-  constructor() {
-    super(allTokens)
-
-    const $ = this
-
-    $.RULE("httpFile", () => {
-      $.MANY_SEP({
-        SEP: Newline,
-        DEF: () => {
-          $.SUBRULE($.request)
-        }
-      })
-    })
-
-    $.RULE("request", () => {
-      $.SUBRULE($.requestLine)
-      $.MANY(() => {
-        $.SUBRULE($.header)
-      })
-      $.OPTION(() => {
-        $.CONSUME(Newline)
-        $.CONSUME(TestSeparator)
-      })
-    })
-
-    $.RULE("requestLine", () => {
-      $.CONSUME(Method)
-      $.CONSUME(Space)
-      $.CONSUME(Url)
-      $.CONSUME(Newline)
-    })
-
-    $.RULE("header", () => {
-      $.CONSUME(HeaderKey)
-      $.CONSUME(HeaderSeparator)
-      $.CONSUME(HeaderValue)
-      $.CONSUME(Newline)
-    })
-
-    this.performSelfAnalysis()
-  }
-}
+const lexer = moo.compile({
+  WS: /[ \t]+/,
+  comment: /#[^\r\n]*/,
+  // number: /0|[1-9][0-9]*/,
+  // string: /"(?:\\["\\]|[^\n"\\])*"/,
+  // lparen: '(',
+  // rparen: ')',
+  // keyword: ['while', 'if', 'else', 'moo', 'cows'],
+  NL: { match: /[\r\n]+/, lineBreaks: true },
+})
 
 export default async (inputText) => {
-  const parser = new HttpParser()
+  lexer.reset(inputText)
 
-  const lexingResult = HttpLexer.tokenize(inputText)
-
-  if (lexingResult.errors.length > 0) {
-    console.error(ngResult.errors.map(({ message }) => message).join('\n'))
-
-    process.exit(1)
+  for (const tk of lexer) {
+    console.log(tk)
   }
-
-  parser.input = lexingResult.tokens
-
-  const cst = parser.httpFile()
-
-  if (parser.errors.length > 0) {
-    console.error(parser.errors.map(({ message }) => message).join('\n'))
-
-    process.exit(1)
-  }
-
-  console.log(cst)
-
-  return cst
 }
