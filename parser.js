4147d7f5fe44199fbb096043c8e9a2ef82576ee0
Andre Vale <andreventuravale@gmail.com>
2024-04-09 15:52:27 -0300

    parser.js

--- parser.js
+++ parser.js
@@ -3,6 +3,8 @@ import moo from 'moo'
 const lexer = moo.compile({
   WS: /[ \t]+/,
   comment: /#[^\r\n]*/,
+  endpoint: /(?:GET|POST|PUT|HEAD|OPTIONS|PATCH|DELETE)\s+(?:[^\r\n]+)/,
+  header: /(?:[a-z_][a-z0-9\-_]*): (?:[^\r\n]*)/,
   // number: /0|[1-9][0-9]*/,
   // string: /"(?:\\["\\]|[^\n"\\])*"/,
   // lparen: '(',
