5214e4b295b6fc22c4ebcf65aaf4dee13685bf66
Andre Vale <andreventuravale@gmail.com>
2024-04-09 14:05:20 -0300

    index.js package.json pnpm-lock.yaml

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env -S node --no-warnings
+
+import { globby } from 'globby'
+
+const files = await globby(['**/*.http'], {
+  cwd: process.pwd
+})
+
+for (const file of files) {
+
+  const file = readFile()
+
+}
