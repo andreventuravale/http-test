4afaf4af0679c77bb4702493bcb44ea3396da384
Andre Vale <andreventuravale@gmail.com>
2024-04-10 11:44:40 -0300

    jest-transformer.test.js

--- jest-transformer.test.js
+++ jest-transformer.test.js
@@ -1,11 +1,14 @@
 import { interpolate } from './jest-transformer.js'
 
-test('interpolation', () => {
+test('nothing to do', () => {
   expect(interpolate(' ')).toMatchInlineSnapshot(`" "`)
   expect(interpolate('')).toMatchInlineSnapshot(`""`)
   expect(interpolate()).toMatchInlineSnapshot(`""`)
   expect(interpolate(null)).toMatchInlineSnapshot(`""`)
   expect(interpolate(undefined)).toMatchInlineSnapshot(`""`)
+})
+
+test('environment variables', () => {
   process.env.FOO = 'bar'
   expect(interpolate(' {{$processEnv FOO}} ')).toMatchInlineSnapshot(`" bar "`)
 })
