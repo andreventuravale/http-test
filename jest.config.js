a1533e2702bd3fd7f47f9e65093a05eb500f85b7
Andre Vale <andreventuravale@gmail.com>
2024-04-10 21:34:02 -0300

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
-
 export default {
   moduleFileExtensions: ['js', 'http'],
-  setupFiles: ["jest.setup.js"],
   testMatch: ['**/*.http', '**/*.test.js'],
   transform: {
     '\\.http$': './jest-transformer.js'
